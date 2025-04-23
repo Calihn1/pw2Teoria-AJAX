d24dcc81ebf2871fa6c7653d4d926fb481a12f82
Calihn1 <ehilacondob@unsa.edu.pe>
2025-04-22 19:11:58 -0500

    agregar toOptionsHtml

--- problema1.js
+++ problema1.js
@@ -4,4 +4,6 @@ $(() => {
   const hideLoader  = () => $('#loader').hide();
   const fetchData   = () => $.ajax({ url: 'data.json', dataType: 'json', beforeSend: showLoader })
                            .always(hideLoader);
-                           
+  const toOptionsHtml = regions => regions
+  .map(r => `<option value="${r}">${r}</option>`)
+  .join('');                        
