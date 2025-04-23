9fe91208c4037ebf0a90785da05899f491b4c111
Calihn1 <ehilacondob@unsa.edu.pe>
2025-04-22 19:09:16 -0500

    implementar fetchData con show/hide loade

--- problema1.js
+++ problema1.js
@@ -2,4 +2,6 @@ $(() => {
   // --- Helpers (puras) ---
   const showLoader  = () => $('#loader').show();
   const hideLoader  = () => $('#loader').hide();
-  
+  const fetchData   = () => $.ajax({ url: 'data.json', dataType: 'json', beforeSend: showLoader })
+                           .always(hideLoader);
+                           
